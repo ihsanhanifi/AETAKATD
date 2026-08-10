4e1ff8c02a7ba16b8bd711c9004bd856133a8897
Ihsan Putra Hanifi <141676833+ihsanhanifi@users.noreply.github.com>
2026-08-10 10:32:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function speakText(text) {
         const utterance = new SpeechSynthesisUtterance(text);
         utterance.lang = 'id-ID'; // Menggunakan bahasa Indonesia
         utterance.rate = 0.5;     // Kecepatan sedikit lebih lambat agar terdengar jelas dan formal
-        utterance.pitch = 4;      // Nada normal
+        utterance.pitch = 1;      // Nada normal
         utterance.volume = 2;     // Volume penuh
         
         window.speechSynthesis.speak(utterance);
